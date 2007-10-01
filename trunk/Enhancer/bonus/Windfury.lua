fae7dcb1f5462dc40946a6fdf59c23a859b1470e
dennis.hafstrom <dennis.hafstrom@13517a02-892e-0410-812e-1d9ab476d632>
2007-10-01 17:37:33 +0000

    Stormstrike check in windfury frame (red/green)

--- trunk/Enhancer/bonus/Windfury.lua
+++ trunk/Enhancer/bonus/Windfury.lua
@@ -16,6 +16,8 @@ function EnhancerWindfury:OnEnable()
 	for _, event in ipairs({ "Damage", "Parry", "Dodge", "Resist", "Absorb", "Block", "Evade"}) do
 		self:RegisterParserEvent({eventType = event}, "ParserInfo");
 	end
+	
+	self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED", "Stormstrike");
 end
 
 function EnhancerWindfury:OnDisable()
@@ -25,17 +27,41 @@ function EnhancerWindfury:OnDisable()
 	self:CancelAllScheduledEvents();
 end
 
+function EnhancerWindfury:Stormstrike(unit, spell, rank)
+	if (totem == Enhancer.BS["Stormstrike"]) then
+		if (not (self:IsEventScheduled("StormstrikeCheck"))) then
+			self:ScheduleRepeatingEvent("StormstrikeCheck", self.StormstrikeCheck, (2 / 10), self);
+		end
+	end
+end
+
+function EnhancerWindfury:StormstrikeCheck()
+	local start, duration = GetSpellCooldown("Stormstrike");
+	if (not start) then return; end
+	
+	if ((start > 0 and duration > 2) or self:IsEventScheduled("WindfuryCooldownNumber")) then
+		Enhancer[FrameName].mainframe:SetBackdropColor(1, (5 / 10), (5 / 10));
+	else
+		Enhancer[FrameName].mainframe:SetBackdropColor((5 / 10), 1, (5 / 10));
+		if (self:IsEventScheduled("StormstrikeCheck")) then
+			self:CancelScheduledEvent("StormstrikeCheck");
+		end
+	end
+end
+
 function EnhancerWindfury:ParserInfo(info)
 	-- Doubt we want both tbh
 	-- if ( (info.abilityName == Enhancer.BS["Windfury Attack"] or info.abilityName == Enhancer.BS["Windfury"]) and info.sourceID == "player" ) then
 	if ( info.abilityName == Enhancer.BS["Windfury Attack"] and info.sourceID == "player" ) then
 		if (Enhancer.debug) then Enhancer:Print(info.abilityName); end
 		
+		--[[
 		if (info.eventType ~= "Damage" and info.eventType ~= "Block") then
 			Enhancer[FrameName].mainframe:SetBackdropColor(1, (5 / 10), (5 / 10));
 		else
 			Enhancer[FrameName].mainframe:SetBackdropColor((5 / 10), 1, (5 / 10));
 		end
+		--]]
 		
 		self:WindfuryHit();
 	end
